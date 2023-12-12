52c42df4e14809365980a6f7a7781219da79b475
Chenwei Zhu <cz1895@nyu.edu>
2023-12-11 23:45:16 -0500

    milestone2

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # FinalProject
 
+# Milestone2
 This code is part of the code of the final project which is a game.
 
 The overall logic of this game is that players start the game by pressing the start button. Once the game begins, a sleeping cat and a fly that wants to bite the cat appear on the screen (the fly is randomly generated and moves towards the cat). If the fly bites the cat, the cat's health bar will decrease. Players need to use a physical joystick to control a hand on the screen to swat the flies and also to keep the cat "warm." Once the game starts, the green sun bar will continuously decrease, and players need to periodically use a photoresistor to restore the sun bar. However, players must be careful in controlling it, because if the sun bar runs out or gets full, the cat's health bar will decrease. If the cat's health bar drops to zero, the game ends and resets.
@@ -10,10 +11,26 @@ This project involves the use of a bidirectional joystick and a photoresistor. I
 The user testing method is quite simple. After completing the basic functionality of the project next week, I will invite people to play the game and provide feedback for improvements.
 
 Overall, this game and the previous reading are related to "The Art of Interactive Design". I am trying to use the game as a way to let users experience and understand the concept I want to convey. Through this game, I hope to let players experience the care of a master towards their pets, awakening people's disdain for pet abandonment.
-
-
-Image on repo:  
+ 
 ![circuit](./circuit.jpg)
 ![FSM](./FSM.jpg)
 ![joyStick](./joyStick.jpg)
+![System diagram](./systemDiagram.jpg)
+
+
+# Milestone3
+This week, I completed the main framework and functionalities of the game. The content I developed includes: finalizing the logic for the "mosquito" enemies, which randomly appear from above and attack the cat through a curved flight path, reducing the cat's health. Players can control a blue ball to crush the mosquitoes and protect the cat. Meanwhile, I figured out how to mix PNG and GIF formats to display our main character, the cat. Now, depending on the game interaction, it has four states: normal, burnt, frozen, and bitten. I also upgraded the joystick control system, eliminating the issue of different speeds in different directions (diagonal being faster than horizontal or vertical). Now, as long as it's outside the dead zone, the ball will move uniformly in the direction of the joystick.
+
+The main body of my game is essentially complete. The next step is to further enhance the game's UI. For instance, adding custom buttons, replacing the red balls representing mosquitoes and the blue balls representing the player with images or animations, and modifying the start and retry pages, etc. I might also need to adjust the game parameters to make the difficulty more appropriate.
+
+
+The logic of cat's display
+![cat states](./catStates.jpg)
+The controller I 3D printed
+![controller](./DIYController.jpg)
+The view of bread board
+![bread board](./breadBoard.jpg)
+Final circuit, FSM and system diagram, same as the old ones since the game framework is confirmed last time already
+![circuit](./circuit.jpg)
+![FSM](./FSM.jpg)
 ![System diagram](./systemDiagram.jpg)
--- sketch.js
+++ sketch.js
@@ -1,30 +1,141 @@
 // Initialize variables
+// For connecting
 let mSerial;
 let connectButton;
 let readyToReceive;
+// For sensor
 let a0Value = 0;  // Value read from the photoreistor
 let joyStickX = 0;  // X-axis value from the joystick
 let joyStickY = 0;  // Y-axis value from the joystick
-
+// Constants for joystick
+const JOYSTICK_DEADZONE = 300;  // Deadzone range
+const JOYSTICK_MID_X = 1960;  // Midpoint X
+const JOYSTICK_MID_Y = 1948;  // Midpoint Y
+// Variables for the cat display
+let catB, catF, catM, catN;  // Cat png&gif
+let catState = 'normal'; // States normal
+let catMFrameCount = 0; // Frame counter for catM GIF
 // Variables for sunBar
 let sunBarLength = 300;  // Initial length of the sunBar
 let sunBarDecreaseSpeed = 1;  // Speed of decrease
 let sunBarIncreaseSpeed = 3 * sunBarDecreaseSpeed;  // Speed of increase
-
 // Variables for healthBar
 let healthBarLength = 300;  // Initial length of the healthBar
 let healthBarDecreaseSpeed = 0.5;  // Speed of decrease in length caused by sunBar
-
 // Variables for controlling the ball
 let ballX, ballY;  // Position of the ball
-let ballSize = 20; 
+let ballSize = 20;
 let ballSpeed = 5;  // Movement speed of the ball
-
 // Game states
 let gameStarted = false;  // If the game has started
 let tryAgain = false;  // If the "Try Again" message should show
 let startButton;  // Button to start the game
+// The mosquitoes array
+let mosquitoes = [];
+let numMosquito = 1; //set number to 1 for now
 
+
+// Function to preload images (GIFs are load in setup using DOM)
+function preload() {
+  catB = loadImage('catB.png');
+  catF = loadImage('catF.png');
+}
+
+// Function to add mosquito (currently a red ball)
+function addMosquito() {
+  let mosquito = {
+    x: random(width), // Starting at a random position on x-axis from the top of the canvas
+    y: 0,             
+    speedX: random(-5, 5), // Random initial speed
+    speedY: random(-5, 5),
+    size: 10          // Size of the mosquito circle
+    // this will be replaced by a png or even a GIF next week, it will change direction according to on the let or right side of the screen so the mosquito is facing the cat
+  };
+  mosquitoes.push(mosquito);
+}
+
+// Functions to update the action of the created mosquito (I want the mosquitos to move towards the cat but in a insect-like curvy way)
+function updateMosquitoes() {
+  for (let i = mosquitoes.length - 1; i >= 0; i--) {
+    let m = mosquitoes[i];
+    // Calculate angle towards the cat so that we can make it move in a constant speed
+    let angle = atan2(600 - m.y, 800 - m.x);
+    let constantSpeed = 2;
+    m.x += constantSpeed * cos(angle);
+    m.y += constantSpeed * sin(angle);
+    // Adding a random speed component
+    m.x += m.speedX;
+    m.y += m.speedY;
+    // Ensure mosquitoes stay within the canvas
+    m.x = constrain(m.x, 0, width);
+    m.y = constrain(m.y, 0, height);
+    // Check collision with the cat
+    if (dist(m.x, m.y, 800, 600) < 140) {
+      mosquitoes.splice(i, 1); // Remove mosquito after collision
+      healthBarLength -= 50;    // Decrease health bar
+      // Change cat state to beingBit
+      catState = 'beingBit';
+      // Set a timer to change the state back to normal after 3.04 seconds
+      setTimeout(() => {
+        catState = 'normal';
+      }, 3040);
+    }
+    // Check collision with the joystick-controlled ball
+    if (dist(m.x, m.y, ballX, ballY) < 50) {
+      mosquitoes.splice(i, 1); // Remove mosquito after collision
+    }
+  }
+  // Change mosquitoes' random speed component every 1/3 second (20 frames)
+  if (frameCount % 20 == 0) {
+    mosquitoes.forEach(m => {
+      m.speedX = random(-5, 5);
+      m.speedY = random(-5, 5);
+    });
+  }
+  // Maintain the number of mosquitoes on the canvas
+  while (mosquitoes.length < numMosquito) {
+      addMosquito();
+  }
+}
+// Function to draw mosquitoes (this part will be edited to be png or gif)
+function drawMosquitoes() {
+    fill(255, 0, 0);
+    mosquitoes.forEach(m => {
+        ellipse(m.x, m.y, m.size, m.size);
+    });
+}
+// Function to display the cat-related gif and png
+function drawCat() {
+  imageMode(CENTER);
+  // Hide GIF images at first
+  catM.hide();
+  catN.hide();
+  // Set cases for switching displays (states) of the cat
+  switch (catState) {
+    case 'normal':
+      // The logic is this, all states beside the beingBit is normal case
+      if (sunBarLength === 300) {//sun bar full
+        image(catB, 800, 600, 391, 391); // Burn
+      } else if (sunBarLength === 0) {//sun bar empty
+        image(catF, 800, 600, 391, 391); // Frozen
+      } else {
+        catN.show(); // Normal state
+        catN.position(800 - 391 / 2, 600 - 391 / 2);
+      }
+      break;
+
+    case 'beingBit':
+      catM.show();//show the beingBit GIF
+      catM.position(800 - 391 / 2, 600 - 391 / 2);
+      if (catMFrameCount < 180) {
+        catMFrameCount++;
+      } else {
+        catState = 'normal'; // Change back to normal state
+        catMFrameCount = 0; // Reset frame counter
+      }
+      break;
+  }
+}
 // Function for serial data
 function receiveSerial() {
   let line = mSerial.readUntil("\n");
@@ -61,6 +172,14 @@ function setup() {
   ballX = width / 2;
   ballY = height / 2;
 
+// Initialize GIFs in setup
+catM = createImg('catM.gif');
+catN = createImg('catN.gif');
+
+catM.hide();  // Initially hide GIFs
+catN.hide();
+
+
   // Setup serial communication
   mSerial = createSerial();
 
@@ -73,11 +192,15 @@ function setup() {
   startButton = createButton('Start');
   startButton.position(width / 2 - 30, height / 2 - 15);
   startButton.mousePressed(startGame); 
+
+  for (let i = 0; i < numMosquito; i++) {
+    addMosquito();
+}
 }
 
 function draw() {
   if (!gameStarted) {
-    background(0);  // Set background to black when game is not started
+    background(0);  // Set background to black when game is not started (will be updated next week with a gif or jpg)
 
     // Show "Try Again" message if the user lost the game
     if (tryAgain) {
@@ -86,10 +209,10 @@ function draw() {
       textAlign(CENTER, CENTER);
       text("Try Again", width / 2, height / 2);
     }
-    return;  // If lost, skip the rest of the draw function and restart the game
+    return;  // If user lost, skip the rest of the draw function and restart the game
   }
 
-  // Set background to white when game is started
+  // Set background to white when game is started (will be updated next week with a png or gif)
   background(255);
 
   // Serial communication
@@ -104,11 +227,18 @@ function draw() {
     receiveSerial();
   }
 
-  // Display sensor data on the screen
+  // Display sensor data on the screen (will be removed in the final version)
   fill(0);
   textSize(16);
   text(`Photoreistor Value: ${a0Value}\nJoystick X: ${joyStickX}, Y: ${joyStickY}`, 10, 20);  // Display all sensor values from Arduuino
-
+//starts to call all the functions that we created
+  if (gameStarted) {
+    updateBallPosition();
+  }
+  updateMosquitoes();
+  drawMosquitoes();
+  drawCat();
+  //Game UI
   // sunBar (not samba)
   if (sunBarLength < 300 && a0Value > 200) {
     sunBarLength += sunBarIncreaseSpeed;  // Increase length if "sun shines" and the bar is not full
@@ -116,12 +246,10 @@ function draw() {
     sunBarLength -= sunBarDecreaseSpeed;  // Otherwise decrease length
   }
   sunBarLength = constrain(sunBarLength, 0, 300);  // Ensure length stays within max value
-
-  // Draw sunBar
+  // Drawing sunBar
   fill(sunBarLength >= 300 ? color(165, 42, 42) : color(0, 255, 0));  // Color is based on length
   noStroke();
   rect(50, height - 80, sunBarLength, 20);
-
   // Draw border for sunBar
   stroke(0);
   noFill(); 
@@ -132,45 +260,41 @@ function draw() {
     healthBarLength -= healthBarDecreaseSpeed;  // Decrease length if sunBar is empty or full
   }
   healthBarLength = constrain(healthBarLength, 0, 300);
-
   // Draw healthBar
   fill(255, 0, 0);
   noStroke();
   rect(50, height - 110, healthBarLength, 20);
-
   // Draw border for healthBar
   stroke(0);
   noFill();
   rect(50, height - 110, 300, 20);
-
   // If health bar empty game over, reset game
   if (healthBarLength <= 0) {
     resetGame();
   }
 
-  // Ball movement
-  if (joyStickX < 1000) {
-    ballX -= ballSpeed;  // Move ball left
-  } else if (joyStickX > 2000) {
-    ballX += ballSpeed;  // Move ball right
+  // Function to update the ball's position
+function updateBallPosition() {
+  let dx = joyStickX - JOYSTICK_MID_X;  // X-axis offset
+  let dy = joyStickY - JOYSTICK_MID_Y;  // Y-axis offset
+  // Calculate the distance from the midpoint
+  let distance = Math.sqrt(dx * dx + dy * dy);
+  if (distance > JOYSTICK_DEADZONE) {
+    // Calculate the angle
+    let angle = Math.atan2(dy, dx);
+    // Update the ball's position based on the angle and speed
+    ballX += ballSpeed * Math.cos(angle);
+    ballY += ballSpeed * Math.sin(angle);
+    // Ensure the ball stays within the canvas
+    ballX = constrain(ballX, ballSize / 2, width - ballSize / 2);
+    ballY = constrain(ballY, ballSize / 2, height - ballSize / 2);
   }
-
-  if (joyStickY < 1000) {
-    ballY -= ballSpeed;  // Move ball up
-  } else if (joyStickY > 2000) {
-    ballY += ballSpeed;  // Move ball down
-  }
-
-  // Make sure ball stays within the canvas
-  ballX = constrain(ballX, ballSize / 2, width - ballSize / 2);
-  ballY = constrain(ballY, ballSize / 2, height - ballSize / 2);
-
-  // Draw the ball
+}
+  // Draw the ball (Will be updated next week as a png or gif, if have time, change to different state after hitting mosquito)
   fill(0, 0, 255);
   noStroke();
   ellipse(ballX, ballY, ballSize, ballSize);
 }
-
 // Start game status
 function startGame() {
   gameStarted = true;  // Set game state to started
@@ -179,7 +303,6 @@ function startGame() {
   healthBarLength = 300;  // Reset healthBar length
   startButton.hide();  // Hide the start button
 }
-
 // Reset of the game statuus
 function resetGame() {
   gameStarted = false;  // Set game to not started
@@ -187,4 +310,6 @@ function resetGame() {
   sunBarLength = 300;  // Reset sunBar length
   healthBarLength = 300;  // Reset healthBar length
   startButton.show();  // Show the start button for a round of new game
+  catM.hide();  // Initially hide GIFs
+  catN.hide();
 }
